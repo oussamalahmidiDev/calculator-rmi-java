3ebfcaf34523fe954a1c989af7b533ec13816fd9
OussamaDev <oussama.lahmidi@icloud.com>
2020-11-25 14:30:10 +0100

    Added junit dependencies in POM

--- logic/src/test/java/com/learn/LogicTests.java
+++ logic/src/test/java/com/learn/LogicTests.java
@@ -1,4 +1,20 @@
 package com.learn;
 
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class LogicTests {
+
+
+    @Test
+    public void operationServiceWillReturnCorrectValueTest() {
+        assertThat(OperationService.calculate(new Operation(1, 2, Operation.OperationType.ADD)))
+            .isEqualTo(3);
+        assertThat(OperationService.calculate(new Operation(1, 2, Operation.OperationType.MULT)))
+            .isEqualTo(2);
+        assertThat(OperationService.calculate(new Operation(1, 2, Operation.OperationType.SUB)))
+            .isEqualTo(-1);
+    }
 }
